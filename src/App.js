48979be1636651e0f674e03b5d40b80a0c4fffaa
david-aguiyi <davidaguiyi9@gmail.com>
2022-03-24 01:26:37 +0100

    added the marked libary and created a markdown const

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { marked } from "marked"
+ 
 
 class App extends React.Component {
   state = {
-      markdown: ''
+      text: ''
     }
 
   handleChange = (event) => {
     this.setState({
-      text: event.target.value
+      text: event.target.value 
     })
   }  
 
   render() {  
     const {text} = this.state;
-
+    const markdown = marked(text)
     return(
-        <div class="container">
-          <h1 className="text-center" >Welcome to React</h1>
-          <div className="row">
-            <h6>Enter your markdown</h6>
-            <div className="col-6">
-              <textarea id='editor' className="form-control" value={text} onChange={this.handleChange}></textarea>
-            </div>
-            <div className="col-6 m-auto" id="preview">
-              <h6>See the result</h6>
-              <div className="preview">
-                  <p>See your preview</p>
+      <div className='App'>
+          <div class="container">
+            <h1 className="text-center" >Welcome to React</h1>
+            <div className="row">
+              <h6>Enter your markdown</h6>
+              <div className="col-6">
+                <textarea id='editor' className="form-control" value={text} onChange={this.handleChange}></textarea>
+              </div>
+
+              <div className="col-6 m-auto" id="preview">
+                <h6 className='text-center'>See the result</h6>
+                <div className="preview">
+                    <p>See your preview</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          </div>
     )
   }
 }
