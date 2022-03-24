0813abc8235317fc06f62f6180fc844bf3f147a6
david-aguiyi <davidaguiyi9@gmail.com>
2022-03-24 10:29:56 +0100

    made initial text = initialstate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-const initialState = {
-  
-}
-
 import React from 'react';
-
 import './App.css';
 import { marked } from "marked"
  
+const initialState = `
+  # Hi Welcome to my Markdown Previewer
+  ## Created by David Aguiyi
+
+  - Hear will
+  - Contain list
+  - items
+
+  [Visit this website to check out my calculator app](https://david-aguiyi.github.io/simple-calculator-app/)
+  
+  This is my inline \`<div></div>\`
+
+  This is a block of code
+
+  \`\`\`
+    let x = 1;
+    let y = 2;
+    let z = x + y
+  \`\`\`
+  ![https://cdn-media-1.freecodecamp.org/images/1*Z3SfTvHOfvZiz1mr7uOODA.jpeg]
+  `;
 
 class App extends React.Component {
   state = {
-      text: ''
+      text: initialState
     }
 
   handleChange = (event) => {
@@ -29,7 +45,7 @@ class App extends React.Component {
           <div className="row">
             <h6>Enter your markdown</h6>
             <div className="col-6">
-              <textarea id='editor' className="form-control" value={text} onChange={this.handleChange}></textarea>
+              <textarea id='editor'  value={text} onChange={this.handleChange}></textarea>
             </div>
 
             <div className="col-6 m-auto" id="preview">
