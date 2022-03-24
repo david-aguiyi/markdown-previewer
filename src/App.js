425491911149b614d4b340855757c1df8ac928f8
david-aguiyi <davidaguiyi9@gmail.com>
2022-03-24 10:54:59 +0100

    updated initialState

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import { marked } from "marked"
  
 const initialState = `
-  # Hi Welcome to my Markdown Previewer
-  ## Created by David Aguiyi
+  Hi Welcome to my Markdown Previewer. **Created by David Aguiyi**
+  # Heading 1
+  ## Heading 2
 
-  - Hear will
+  - Here will
   - Contain list
   - items
 
@@ -14,14 +15,14 @@ const initialState = `
   
   This is my inline \`<div></div>\`
 
-  This is a block of code
+  >his is a block of code
 
   \`\`\`
     let x = 1;
     let y = 2;
     let z = x + y
   \`\`\`
-  ![https://cdn-media-1.freecodecamp.org/images/1*Z3SfTvHOfvZiz1mr7uOODA.jpeg]
+  ![Keep coding](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fcoding&psig=AOvVaw0uRtZGkAs0HKQ2j8pkmipT&ust=1648201129053000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDN0uq53vYCFQAAAAAdAAAAABAN)
   `;
 
 class App extends React.Component {
@@ -45,12 +46,12 @@ class App extends React.Component {
           <div className="row">
             <h6>Enter your markdown</h6>
             <div className="col-6">
-              <textarea id='editor'  value={text} onChange={this.handleChange}></textarea>
+              <textarea id='editor' className="form-control" value={text} onChange={this.handleChange}></textarea>
             </div>
 
             <div className="col-6 m-auto" id="preview">
               <h6 className='text-center'>See the result</h6>
-              <div className="preview rounded"
+              <div className="preview container"
                   dangerouslySetInnerHTML={{__html:markdown}} />    
                 <p>this is the previewer</p>
             </div>
