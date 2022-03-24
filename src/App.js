0de7e3f0080ff92768b90e5610c55cee485ec66c
david-aguiyi <davidaguiyi9@gmail.com>
2022-03-24 22:33:03 +0100

    removed text-center class from preview header

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends React.Component {
             </div>
 
             <div className="col-6 m-auto" >
-              <h6 className='text-center'>Here's your Preview</h6>
+              <h6>Here's your Preview</h6>
               <div className="preview">
                 <div dangerouslySetInnerHTML={{__html:markdown}} id="preview" />    
               </div>
